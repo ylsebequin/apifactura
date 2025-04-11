2a4338bbf0825d72b2be148ffbfa6a598a12cd13
ylsebequin <ylsebenitez21@gmail.com>
2025-04-10 22:13:26 -0300

    venta update

--- app/Http/Controllers/VentaController.php
+++ app/Http/Controllers/VentaController.php
@@ -113,17 +113,77 @@ class VentaController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Venta $venta)
-    {
-        //
-    }
+    public function edit(Venta $venta) {}
 
     /**
      * Update the specified resource in storage.
      */
     public function update(Request $request, Venta $venta)
     {
-        //
+        try {
+
+            DB::beginTransaction();
+            if ($venta->estado == 'pagada') {
+                return response()->json($this->get_response("La venta ya fue pagada, no se puede modificar", 500, null), 500);
+            }
+
+            $data = $request->validate([
+                'cliente_id' => 'required|exists:clientes,id',
+                'fecha_venta' => 'required|date',
+                'productos' => 'required|array',
+                'productos.*.producto_id' => 'required|exists:productos,id',
+                'productos.*.cantidad' => 'required|integer|min:1',
+            ]);
+
+            foreach ($venta->detalles as $detalle) {
+                $productoInfo = Productos::find($detalle->producto_id);
+                $productoInfo->update([
+                    'stock' => $productoInfo->stock + $detalle->cantidad
+                ]);
+            }
+
+            $venta->detalles()->delete();
+
+            $total = 0;
+
+            foreach ($data['productos'] as $producto) {
+
+                $productoInfo = Productos::find($producto['producto_id']);
+
+                if ($productoInfo->stock < $producto['cantidad']) {
+                    return response()->json($this->get_response("No hay suficiente stock para el producto", 500, null), 500);
+                }
+
+                $subtotal = $productoInfo->precio * $producto['cantidad'];
+
+                $total += $subtotal;
+
+                VentaDetalle::create([
+                    'venta_id' => $venta->id,
+                    'producto_id' => $producto['producto_id'],
+                    'cantidad' => $producto['cantidad'],
+                    'precio' => $productoInfo->precio,
+                    'subtotal' => $subtotal,
+                ]);
+
+                $productoInfo->update([
+                    'stock' => $productoInfo->stock - $producto['cantidad']
+                ]);
+            }
+
+            $venta->update([
+                'total_venta' => $total,
+                'estado' => 'pagada'
+            ]);
+
+            DB::commit();
+
+            return response()->json($this->get_response("Venta actualizada con exito", 200, $venta), 200);
+        } catch (Exception $e) {
+
+            DB::rollBack();
+            return response()->json($this->get_response($e->getMessage(), 500, null), 500);
+        }
     }
 
     /**
